936969891f3dcefdc0313be92a8e6d5afe3d88a2
Steven Scott <stevescott2653@gmail.com>
2019-10-07 14:35:55 -0400

    Add files via upload

---
+++ Swap_two_numbers_without_third_variable/Swap_two_numbers_without_third_variable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swap_two_numbers_without_third_variable
+{
+    public class SwapExample
+    {
+        public static void Main(string[] args)
+        {
+            int a = 5, b = 10;
+            Console.WriteLine("Before swap a= "+a+" b= "+b);
+            a = a * b; // a=50(5*10)
+            b = a / b; // b=5(50/10)
+            a = a / b; // a=10(50/5)
+            Console.WriteLine("After swap a= "+a+" b= "+b);
+        }
+            
+    }
+}
---
+++ Swap_two_numbers_without_third_variable/Swap_two_numbers_without_third_variable/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Swap_two_numbers_without_third_variable")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Swap_two_numbers_without_third_variable")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("17b78bf9-8024-4315-ab0d-56b87566f750")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
